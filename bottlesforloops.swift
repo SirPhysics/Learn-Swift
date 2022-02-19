278e6ab5ed5be6cc379395984d4f1e11024a5bcf
James Kayik Choi <62376291+SirPhysics@users.noreply.github.com>
2022-02-19 16:28:09 +1100

    Create bottlesforloops.swift

---
+++ bottlesforloops.swift
@@ -0,0 +1,7 @@
+for numBottles in stride(from:99, to:1, by:-1) {
+  print("\(numBottles) bottles of milk on the wall, \(numBottles) bottles of milk!")
+  print("You take one down and pass it around…")
+  print("\(numBottles) bottles of milk on the wall!\n")
+}
+print("0 bottles of milk on the wall, 0 bottles of milk!\nGo to the store and buy some more,\n99 bottles of milk on the wall!")
+
