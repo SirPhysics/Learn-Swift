f3fe78798c62bdc11da3c208b0f83f93bc05c6b3
James Kayik Choi <62376291+SirPhysics@users.noreply.github.com>
2022-02-21 10:21:41 +1100

    Create Palindrome.swift

---
+++ Arrays&Sets/Palindrome.swift
@@ -0,0 +1,13 @@
+var text = ["m", "a", "d","a","m"]
+var reversed = [String]()
+var counter = text.count - 1 
+
+while counter >= 0 {
+  reversed.append(text[counter])
+  counter -= 1
+}
+if text == reversed {
+  print("We have a palindrome!")
+} else {
+  print("We don't have a palindrome")
+}
